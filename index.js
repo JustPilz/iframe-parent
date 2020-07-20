ebe4749b5d4c079977ce71f7c8b8421a66102ae0
Dmitry Krivyakov <dkrivyakov@alfabank.ru>
2020-07-20 17:44:22 +0300

    up

---
+++ index.js
@@ -0,0 +1,12 @@
+function openFrame() {
+  const firstname = document.getElementById("firstname").value;
+  const lastname = document.getElementById("lastname").value;
+
+  document.getElementById(
+    "frame"
+  ).src = `https://justpilz.github.io/iframe-child/?firstname=${firstname}&lastname=${lastname}`;
+}
+
+function reloadFrame() {
+  document.getElementById("frame").contentWindow.location.reload(true);
+}
